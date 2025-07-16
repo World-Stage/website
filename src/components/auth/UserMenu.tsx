cdf198ef51b104a73063d5af279a43dcac283d6e
John Fletcher <johnfletcherdev@gmail.com>
2025-07-16 16:33:38 -0600

    removed stuf

--- src/components/auth/UserMenu.tsx
+++ src/components/auth/UserMenu.tsx
@@ -72,28 +72,9 @@ export function UserMenu({ onShowAuthModal }: UserMenuProps) {
 
           {/* Menu Items */}
           <div className="py-1">
-            {/* Language Option */}
-            <button className="flex items-center w-full px-4 py-3 text-left text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-              <GlobeAltIcon className="w-5 h-5 mr-3" />
-              <span>Language</span>
-              <ArrowRightIcon className="w-4 h-4 ml-auto" />
-            </button>
 
             {/* Theme Toggle */}
             <DropdownThemeToggle />
-
-            {/* Opt-out Preferences */}
-            <button className="flex items-center w-full px-4 py-3 text-left text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-              <InformationCircleIcon className="w-5 h-5 mr-3" />
-              <span>Opt-out Preferences</span>
-            </button>
-
-            {/* Labeled Content */}
-            <button className="flex items-center w-full px-4 py-3 text-left text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-              <TagIcon className="w-5 h-5 mr-3" />
-              <span>Labeled Content</span>
-              <ArrowRightIcon className="w-4 h-4 ml-auto" />
-            </button>
 
             {/* Separator */}
             <div className="border-t border-gray-200 dark:border-gray-700 my-1"></div>
